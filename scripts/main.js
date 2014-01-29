47fe8456d71f947397aab2dd4b7e87b98e30504a
Anuj Kothari <anujckothari@gmail.com>
2014-01-30 03:32:49 +0530

    Added features …forgot password …edit and view profile
    Removed facebook
    Small ui updates

--- scripts/main.js
+++ scripts/main.js
@@ -56,7 +56,7 @@ $(document).ready(function() {
 			   // Swipe script //
 /**************************************************/	
 
-$("#content").swipe({
+/*$("#content").swipe({
 	triggerOnTouchEnd : true,
 		allowPageScroll:"vertical",
 		swipe:function(event, direction, distance, duration, fingerCount)
@@ -75,7 +75,114 @@ $("#content").swipe({
 		},
 		threshold:180
 		
-	})
+	})*/
+
+/**************************************************/
+			   // Profile form submissions //
+/**************************************************/	
+
+	
+	$("#personalFormSubmit").click( function () {
+		//window.scrollTo(0,0);
+		$('#personalStatus').fadeIn(100).html("Saving your information...");    
+		//$('#php_profile_userprofile').fadeIn(100).html("<img src='http://artistivity.com/images/loader.gif' style='float:left' />");
+		//alert("coming here");
+		$.post('http://artistivity.com/new/mobile/updatePersonal.php',
+			$("#personalForm").serialize(),
+			function(data){
+			$("#php_profile_userprofile").html(data)
+				//console.log(data);
+				if (data.toLowerCase().indexOf("success") >= 0)
+				{
+					$('#personalStatus').fadeIn(100).html("Information saved");    
+				}
+				else
+				{
+					$('#personalStatus').fadeIn(100).html("An error occured. Please try again.");    
+				}
+			}
+		);
+	    return false;   
+	});
+	
+	$("#talentFormSubmit").click( function () {
+		//window.scrollTo(0,0);
+		$('#talentStatus').fadeIn(100).html("Saving your information...");    
+		//$('#php_profile_userprofile').fadeIn(100).html("<img src='http://artistivity.com/images/loader.gif' style='float:left' />");
+		//alert("coming here");
+		$.post('http://artistivity.com/new/mobile/updateTalent.php',
+			$("#talentForm").serialize(),
+			function(data){
+			$("#php_profile_userprofile").html(data)
+				console.log(data);
+				if (data.toLowerCase().indexOf("success") >= 0)
+				{
+					$('#talentStatus').fadeIn(100).html("Information saved");
+				}
+				else
+				{
+					$('#talentStatus').fadeIn(100).html("An error occured. Please try again.");    
+				}
+			}
+		);
+	    return false;   
+	});
+	
+	$("#feeFormSubmit").click( function () {
+		//window.scrollTo(0,0);
+		$('#feeStatus').fadeIn(100).html("Saving your information...");    
+		//$('#php_profile_userprofile').fadeIn(100).html("<img src='http://artistivity.com/images/loader.gif' style='float:left' />");
+		//alert("coming here");
+		$.post('http://artistivity.com/new/mobile/updateFee.php',
+			$("#feeForm").serialize(),
+			function(data){
+			$("#php_profile_userprofile").html(data)
+				console.log(data);
+				if (data.toLowerCase().indexOf("success") >= 0)
+				{
+					$('#feeStatus').fadeIn(100).html("Information saved");
+				}
+				else
+				{
+					$('#feeStatus').fadeIn(100).html("An error occured. Please try again.");    
+				}
+			}
+		);
+	    return false;   
+	});
+	
+	$("#preferencesFormSubmit").click( function () {
+		//window.scrollTo(0,0);
+		$('#preferencesStatus').fadeIn(100).html("Saving your information...");    
+		//$('#php_profile_userprofile').fadeIn(100).html("<img src='http://artistivity.com/images/loader.gif' style='float:left' />");
+		//alert("coming here");
+		$.post('http://artistivity.com/new/mobile/updatePreferences.php',
+			$("#preferencesForm").serialize(),
+			function(data){
+			$("#php_profile_userprofile").html(data)
+				console.log(data);
+				if (data.toLowerCase().indexOf("success") >= 0)
+				{
+					$('#preferencesStatus').fadeIn(100).html("Information saved");
+				}
+				else
+				{
+					$('#preferencesStatus').fadeIn(100).html("An error occured. Please try again.");    
+				}
+			}
+		);
+	    return false;   
+	});
+	
+	$("#editProfileButton").click( function(){
+		$("#tab01-view").hide(500);
+		$("#tab01-edit").show(500);
+	});
+	
+	$(".moreLink01").click( function(){
+		$(".morebio").show();
+		$(this).hide();
+	});
 
 	
 // End jQuery

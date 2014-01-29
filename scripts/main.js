3d2a5c644ab0e0362ac793a1c4debb4832a65f4a
Anuj Kothari <anujckothari@gmail.com>
2014-01-30 04:29:49 +0530

    no message

--- scripts/main.js
+++ scripts/main.js
@@ -51,6 +51,109 @@ $(document).ready(function() {
 			$(this).next('div.table-wrapper').stop(true, true).slideDown();
 		}
 	})
+	
+	$("#editProfileButton").live('click', function(){
+		$("#tab01-view").hide(500);
+		$("#tab01-edit").show(500);
+	})
+	
+	$("#personalFormSubmit").live('click', function () {
+		//window.scrollTo(0,0);
+		$('#personalStatus').fadeIn(100).html("Saving your information...");    
+		//$('#php_profile_userprofile').fadeIn(100).html("<img src='http://artistivity.com/images/loader.gif' style='float:left' />");
+		//alert("coming here");
+		$.post('http://artistivity.com/new/mobile/updatePersonal.php',
+			$("#personalForm").serialize(),
+			function(data){
+			$("#php_profile_userprofile").html(data)
+				//console.log(data);
+				if (data.toLowerCase().indexOf("success") >= 0)
+				{
+					$('#personalStatus').fadeIn(100).html("Information saved");    
+				}
+				else
+				{
+					$('#personalStatus').fadeIn(100).html("An error occured. Please try again.");    
+				}
+			}
+		);
+	    return false;   
+	})
+	
+	$("#talentFormSubmit").live('click', function () {
+		//window.scrollTo(0,0);
+		$('#talentStatus').fadeIn(100).html("Saving your information...");    
+		//$('#php_profile_userprofile').fadeIn(100).html("<img src='http://artistivity.com/images/loader.gif' style='float:left' />");
+		//alert("coming here");
+		$.post('http://artistivity.com/new/mobile/updateTalent.php',
+			$("#talentForm").serialize(),
+			function(data){
+			$("#php_profile_userprofile").html(data)
+				console.log(data);
+				if (data.toLowerCase().indexOf("success") >= 0)
+				{
+					$('#talentStatus').fadeIn(100).html("Information saved");
+				}
+				else
+				{
+					$('#talentStatus').fadeIn(100).html("An error occured. Please try again.");    
+				}
+			}
+		);
+	    return false;   
+	})
+	
+	$("#feeFormSubmit").live('click', function () {
+		//window.scrollTo(0,0);
+		$('#feeStatus').fadeIn(100).html("Saving your information...");    
+		//$('#php_profile_userprofile').fadeIn(100).html("<img src='http://artistivity.com/images/loader.gif' style='float:left' />");
+		//alert("coming here");
+		$.post('http://artistivity.com/new/mobile/updateFee.php',
+			$("#feeForm").serialize(),
+			function(data){
+			$("#php_profile_userprofile").html(data)
+				console.log(data);
+				if (data.toLowerCase().indexOf("success") >= 0)
+				{
+					$('#feeStatus').fadeIn(100).html("Information saved");
+				}
+				else
+				{
+					$('#feeStatus').fadeIn(100).html("An error occured. Please try again.");    
+				}
+			}
+		);
+	    return false;   
+	})
+	
+	$("#preferencesFormSubmit").live('click', function () {
+		//window.scrollTo(0,0);
+		$('#preferencesStatus').fadeIn(100).html("Saving your information...");    
+		//$('#php_profile_userprofile').fadeIn(100).html("<img src='http://artistivity.com/images/loader.gif' style='float:left' />");
+		//alert("coming here");
+		$.post('http://artistivity.com/new/mobile/updatePreferences.php',
+			$("#preferencesForm").serialize(),
+			function(data){
+			$("#php_profile_userprofile").html(data)
+				console.log(data);
+				if (data.toLowerCase().indexOf("success") >= 0)
+				{
+					$('#preferencesStatus').fadeIn(100).html("Information saved");
+				}
+				else
+				{
+					$('#preferencesStatus').fadeIn(100).html("An error occured. Please try again.");    
+				}
+			}
+		);
+	    return false;   
+	})
+
+	
+	$(".moreLink01").live('click', function(){
+		$(".morebio").show();
+		$(this).hide();
+	})
 
 /**************************************************/
 			   // Swipe script //
@@ -83,105 +186,3 @@ $(document).ready(function() {
 	
 // End jQuery
 });
-
-$("#personalFormSubmit").click( function () {
-		//window.scrollTo(0,0);
-		$('#personalStatus').fadeIn(100).html("Saving your information...");    
-		//$('#php_profile_userprofile').fadeIn(100).html("<img src='http://artistivity.com/images/loader.gif' style='float:left' />");
-		//alert("coming here");
-		$.post('http://artistivity.com/new/mobile/updatePersonal.php',
-			$("#personalForm").serialize(),
-			function(data){
-			$("#php_profile_userprofile").html(data)
-				//console.log(data);
-				if (data.toLowerCase().indexOf("success") >= 0)
-				{
-					$('#personalStatus').fadeIn(100).html("Information saved");    
-				}
-				else
-				{
-					$('#personalStatus').fadeIn(100).html("An error occured. Please try again.");    
-				}
-			}
-		);
-	    return false;   
-	});
-	
-	$("#talentFormSubmit").click( function () {
-		//window.scrollTo(0,0);
-		$('#talentStatus').fadeIn(100).html("Saving your information...");    
-		//$('#php_profile_userprofile').fadeIn(100).html("<img src='http://artistivity.com/images/loader.gif' style='float:left' />");
-		//alert("coming here");
-		$.post('http://artistivity.com/new/mobile/updateTalent.php',
-			$("#talentForm").serialize(),
-			function(data){
-			$("#php_profile_userprofile").html(data)
-				console.log(data);
-				if (data.toLowerCase().indexOf("success") >= 0)
-				{
-					$('#talentStatus').fadeIn(100).html("Information saved");
-				}
-				else
-				{
-					$('#talentStatus').fadeIn(100).html("An error occured. Please try again.");    
-				}
-			}
-		);
-	    return false;   
-	});
-	
-	$("#feeFormSubmit").click( function () {
-		//window.scrollTo(0,0);
-		$('#feeStatus').fadeIn(100).html("Saving your information...");    
-		//$('#php_profile_userprofile').fadeIn(100).html("<img src='http://artistivity.com/images/loader.gif' style='float:left' />");
-		//alert("coming here");
-		$.post('http://artistivity.com/new/mobile/updateFee.php',
-			$("#feeForm").serialize(),
-			function(data){
-			$("#php_profile_userprofile").html(data)
-				console.log(data);
-				if (data.toLowerCase().indexOf("success") >= 0)
-				{
-					$('#feeStatus').fadeIn(100).html("Information saved");
-				}
-				else
-				{
-					$('#feeStatus').fadeIn(100).html("An error occured. Please try again.");    
-				}
-			}
-		);
-	    return false;   
-	});
-	
-	$("#preferencesFormSubmit").click( function () {
-		//window.scrollTo(0,0);
-		$('#preferencesStatus').fadeIn(100).html("Saving your information...");    
-		//$('#php_profile_userprofile').fadeIn(100).html("<img src='http://artistivity.com/images/loader.gif' style='float:left' />");
-		//alert("coming here");
-		$.post('http://artistivity.com/new/mobile/updatePreferences.php',
-			$("#preferencesForm").serialize(),
-			function(data){
-			$("#php_profile_userprofile").html(data)
-				console.log(data);
-				if (data.toLowerCase().indexOf("success") >= 0)
-				{
-					$('#preferencesStatus').fadeIn(100).html("Information saved");
-				}
-				else
-				{
-					$('#preferencesStatus').fadeIn(100).html("An error occured. Please try again.");    
-				}
-			}
-		);
-	    return false;   
-	});
-	
-	$("#editProfileButton").click( function(){
-		$("#tab01-view").hide(500);
-		$("#tab01-edit").show(500);
-	});
-	
-	$(".moreLink01").click( function(){
-		$(".morebio").show();
-		$(this).hide();
-	});
